2f4fd5d3fccba583ed746a910ff489d08d6dea11
RZZhang6 <zrz240132771@qq.com>
2022-04-29 21:57:42 +0800

    测试1

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author RZZhang
+ * @create 2022-04-29 21:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试GIT");
+
+    }
+
+}
